1ff1d74d7079be30456336b456f5c997d355829b
ktscates <cates2506@gmail.com>
2024-09-15 15:01:15 +0200

    initial commit & project setup

--- src/app/services/api-client.service.spec.ts
+++ src/app/services/api-client.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { ApiClientService } from './api-client.service';
+import { ApiClientService } from './api-client.service'
 
 describe('ApiClientService', () => {
-  let service: ApiClientService;
+  let service: ApiClientService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiClientService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(ApiClientService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
